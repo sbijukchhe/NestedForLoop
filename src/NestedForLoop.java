0dae566d37929d1ba1daa7a66a20e8160741428d
GBTC441013UR <GBTC441013UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-09-06 14:24:16 -0400

    Nested for loop

---
+++ src/NestedForLoop.java
@@ -0,0 +1,26 @@
+/*
+Use nested for loops to produce the following output:
+
+1
+22
+333
+4444
+55555
+666666
+7777777
+88888888
+999999999
+ */
+
+public class NestedForLoop {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
